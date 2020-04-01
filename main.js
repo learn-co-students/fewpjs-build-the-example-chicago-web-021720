5d91b0d7456ff250e67f5ab5ac56fa5c8093c157
GreatDeveloper66 <ashaffe@gmail.com>
2020-04-01 17:43:40 -0500

    Done.

--- main.js
+++ main.js
@@ -4,9 +4,31 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+Array.from(document.querySelectorAll('.like-glyph')).forEach(function(heart){
+  heart.addEventListener('click',function(){
+    if(heart.innerHTML == FULL_HEART){
+      heart.innerHTML = EMPTY_HEART;
+      heart.classList.remove('activated-heart');
+    }
+    else {
+      mimicServerCall()
+        .then(serverMessage => {
+          heart.innerHTML = FULL_HEART;
+          heart.classList.add('activated-heart');
+        })
+        .catch(error => {
+          let modal = document.getElementById('modal');
+          let modalmessage = document.getElementById('modal-message');
+          modal.classList.remove('hidden');
+          modalmessage.innerHTML = error;
+          setTimeout(function(){
+            modal.classList.add('hidden');
+          }, 5000);
+        });
+    }
 
-
-
+  });
+})
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
